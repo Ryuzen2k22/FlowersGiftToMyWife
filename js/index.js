a669717c363b1d324f142b465f6505dfe6d15864
Ryuzen2k22 <111623948+Ryuzen2k22@users.noreply.github.com>
2025-07-23 19:49:06 +0700

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Have Something for U'.split('')
+const text = 'Mas Punya Sesuatu Buat Dede'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
